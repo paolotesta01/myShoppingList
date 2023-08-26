ed08db5aae443fe0e35bfb89409691a87f9e1f85
paolotesta01 <142208411+paolotesta01@users.noreply.github.com>
2023-08-26 14:52:45 +0200

    Update SL.js

--- SL.js
+++ SL.js
@@ -161,11 +161,13 @@ switchSlider.addEventListener("click",()=> {
         hideSlider()
         set (ref(db, "settings/fontSizeBar"), {fontSizeBar : "off"})
         fontSizeBar = "off" 
+        switchSlider.innerHTML = "Input font size"
     }
     else {
         hideSliderTwo()
         set (ref(db, "settings/fontSizeBar"), {fontSizeBar : "on"})
-        fontSizeBar = "on" 
+        fontSizeBar = "on"
+        switchSlider.innerHTML = "Font size bar"
     }
   })
 
